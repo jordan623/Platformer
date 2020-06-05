6ea9050e51c49d0e7a34f0263b822d1164bc3b49
jorda <jorda@192.168.1.37>
2020-06-04 21:45:59 -0500

    Initialized main method

---
+++ Platformer/src/platformer/Platformer.java
@@ -0,0 +1,21 @@
+/*
+ * Jordan B. Sears
+ * Email: jordanbradfordsears@gmail.com
+ * RIP Terry
+ */
+package platformer;
+
+/**
+ *
+ * @author jorda
+ */
+public class Platformer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
